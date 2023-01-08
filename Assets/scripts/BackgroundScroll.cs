97d273a3967759c132270cd33beb22204b98067e
AbaidUllah <abaidu408@gmail.com>
2023-01-08 23:02:26 +0500

    Background Scrolling Complete

---
+++ Assets/scripts/BackgroundScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float speed=0.1f;
+    public Renderer meshRanderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frameor
+    void Update()
+    {
+        // Vector2 offset= meshRanderer.material.mainTextureOffset;
+        // offset=offset+new Vector2(0,speed*Time.deltaTime);
+        // meshRanderer.material.mainTextureOffset=offset;
+
+        meshRanderer.material.mainTextureOffset+=new Vector2(0,speed*Time.deltaTime);
+
+        
+        
+    }
+}
