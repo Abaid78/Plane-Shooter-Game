0e83eb9403fd56c5c19d799d48d410321131ea1c
AbaidUllah <abaidu408@gmail.com>
2023-01-10 23:05:13 +0500

    Added Player Movements

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {   
+    public float speed=10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ///Player Movement using Input Manager
+        //Horizontal Movement(Left Right)
+        float deltaX=Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        float newXpos=transform.position.x+deltaX;
+        transform.position=new Vector2(newXpos,transform.position.y);
+        //Vertical Movement(UP and Down)
+        float deltaY=Input.GetAxis("Vertical")*Time.deltaTime*speed;
+        float newYpos=transform.position.y+deltaY;
+        transform.position=new Vector2(newXpos,newYpos);
     }
 }
