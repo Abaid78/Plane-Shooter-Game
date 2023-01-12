c750df90bf0a7926007a08194ab97e53573077e7
AbaidUllah <abaidu408@gmail.com>
2023-01-12 23:15:25 +0500

    Shoot Automatic

--- Assets/scripts/Shooting.cs
+++ Assets/scripts/Shooting.cs
@@ -7,14 +7,30 @@ public class Shooting : MonoBehaviour
     public GameObject bullet;
     public GameObject spwonPoint1;
     public GameObject spwonPoint2;
- 
+    void Start()
+    {
+        StartCoroutine(Shoot());
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
-            Instantiate(bullet,spwonPoint1.transform.position,Quaternion.identity);
-            Instantiate(bullet,spwonPoint2.transform.position,Quaternion.identity);
+
+
+    }
+    void fire()
+    {
+        Instantiate(bullet, spwonPoint1.transform.position, Quaternion.identity);
+        Instantiate(bullet, spwonPoint2.transform.position, Quaternion.identity);
+    }
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            fire();
+
         }
-        
+
     }
 }
