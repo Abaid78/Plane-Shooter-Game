9c30951b510374e92a25b4f0848bf81931aa8b60
AbaidUllah <abaidu408@gmail.com>
2023-01-12 21:11:40 +0500

    Player Shooting Complete

---
+++ Assets/scripts/Shooting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject spwonPoint1;
+    public GameObject spwonPoint2;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Instantiate(bullet,spwonPoint1.transform.position,Quaternion.identity);
+            Instantiate(bullet,spwonPoint2.transform.position,Quaternion.identity);
+        }
+        
+    }
+}
